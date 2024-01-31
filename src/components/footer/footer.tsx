fff834338997a15033ae5599efa57985a3fc0b14
felipe-jaque-dev <felipealexandrej@gmail.com>
2024-01-31 16:17:45 -0300

    Revisão de rotas realizada, projeto concluido

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,29 +1,25 @@
-import { GithubLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import { GithubLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react';
 
 function Footer() {
- 
-
-
   return (
-    <>
-        <div className="flex justify-center bg-dark-30/10 border-t-2 border-dark-30 text-dark-60/100">
-          <div className="container flex flex-col items-center py-4">
-            <p className='text-xl font-bold'>SuperFarma, a melhor do Brasil! | Copyright: </p>
-            <p className='text-lg'>Acesse nossas redes sociais</p>
-            <div className='flex gap-2'>
-              <a href="https://www.linkedin.com/in/felipemacedo1/" target="_blank" rel="noopener noreferrer">
-                <LinkedinLogo size={36} weight='bold' />
-              </a>
-              <a href="https://www.oficinadanet.com.br/instagram/47773-instagram-motivos-para-desativar" target="_blank" rel="noopener noreferrer">
-                <InstagramLogo size={36} weight='bold' /></a>
-              <a href="https://github.com/FelipeAJdev/farmacia-perfomance-goal-react" target="_blank">
-                <GithubLogo size={36} weight='bold' /></a>
-
-            </div>
-          </div>
+    <div className="bg-blue-500 border-t-2 border-blue-400 text-white">
+      <div className="container flex flex-col items-center py-4">
+        <p className='text-xl font-bold'>SuperFarma, a melhor do Brasil! | Copyright:</p>
+        <p className='text-lg'>Acesse nossas redes sociais</p>
+        <div className='flex gap-2'>
+          <a href="https://www.linkedin.com/in/felipemacedo1/" target="_blank" rel="noopener noreferrer">
+            <LinkedinLogo size={36} weight='bold' />
+          </a>
+          <a href="https://www.oficinadanet.com.br/instagram/47773-instagram-motivos-para-desativar" target="_blank" rel="noopener noreferrer">
+            <InstagramLogo size={36} weight='bold' />
+          </a>
+          <a href="https://github.com/FelipeAJdev/farmacia-perfomance-goal-react" target="_blank">
+            <GithubLogo size={36} weight='bold' />
+          </a>
         </div>
-      </>
-  )
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
